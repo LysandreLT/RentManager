5e02283157455e2b0c4f5d6addd1f0bff71e7e0b
Chambellan <lolfirstangel@gmail.com>
2023-02-17 15:06:13 +0100

    Ajout du prompt d'introduction

--- src/main/java/com/epf/rentmanager/ui/cli/Main.java
+++ src/main/java/com/epf/rentmanager/ui/cli/Main.java
@@ -5,7 +5,11 @@ public class Main {
         while(true){
             System.out.println("Que souhaitez vous faire? \n " +
                     "1 : Créer un client \n " +
-                    "2 : ");
+                    "2 : Afficher la liste de tous les clients \n" +
+                    "3 : Ajouter un véhicule \n" +
+                    "4 : Afficher la liste de tous les véhicules \n" +
+                    "5 : Supprimer un client \n " +
+                    "6 : Supprimer un véhicule");
         }
     }
 }
