8ca60d4051b4a35720f4aa7c3252dba6e90886ed
Chambellan <lolfirstangel@gmail.com>
2023-02-17 15:18:16 +0100

    Ajout de l'input

--- src/main/java/com/epf/rentmanager/ui/cli/Main.java
+++ src/main/java/com/epf/rentmanager/ui/cli/Main.java
@@ -1,11 +1,14 @@
 package com.epf.rentmanager.ui.cli;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         while(true){
             System.out.println("Que souhaitez vous faire? \n " +
                     "1 : Créer un client \n " +
                     "2 : ");
+            System.in.read();
         }
     }
 }
