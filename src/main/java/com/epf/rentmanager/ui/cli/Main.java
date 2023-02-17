717665d893aa1fbe4f3c1a23c08d0aa4945fc682
Chambellan <lolfirstangel@gmail.com>
2023-02-17 15:43:43 +0100

    Ajout de prompts de confirmation

--- src/main/java/com/epf/rentmanager/ui/cli/Main.java
+++ src/main/java/com/epf/rentmanager/ui/cli/Main.java
@@ -1,6 +1,7 @@
 package com.epf.rentmanager.ui.cli;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,7 +13,17 @@ public class Main {
                     "4 : Afficher la liste de tous les véhicules \n" +
                     "5 : Supprimer un client \n " +
                     "6 : Supprimer un véhicule");
-            System.in.read();
+            Scanner scanner = new Scanner(System.in);
+            switch(Integer.parseInt(scanner.nextLine())){
+                case 1:
+                    System.out.println("Ajout d'un nouveau client...");
+
+                case 2:
+                    System.out.println("Liste des clients :");
+
+                case 3:
+                    System.out.println("Ajout d'un véhicule");
+            }
         }
     }
 }
